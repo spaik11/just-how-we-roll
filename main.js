128adedc0c408c65021072c2a303094b6e2a57da
Sung Paik <sung.paik@codeimmersives.com>
2019-11-19 15:46:27 -0500

    completed changing the images and creating functions for each die to change also storing the data in an empty array

--- main.js
+++ main.js
@@ -21,15 +21,55 @@ function getRandomNumber(max) {
     return result;
 }
 
-
 /*******************
  * YOUR CODE BELOW *
  *******************/
+function rollD6() {
+    const result = getRandomNumber(6);
+    sixes.push(result);
+    d6.src = `./images/d6/${result}.png`
+}
 
+function rollDoubleD6() {
+    const result1 = getRandomNumber(6);
+    const result2 = getRandomNumber(6);
 
+    doubleSixes.push(result1 + result2);
+    document.querySelector('#double-d6-roll-1').src = `./images/d6/${result1}.png`
+    document.querySelector('#double-d6-roll-2').src = `./images/d6/${result2}.png`
+}
+
+function rollTwelves() {
+    const result = getRandomNumber(12);
+    twelves.push(result);
+    d12.src = `./images/numbers/${result}.png`
+}
+
+function rollTwenty() {
+    const result = getRandomNumber(20)
+    twelves.push(result);
+    d20.src = `./images/numbers/${result}.png`
+}
 /*******************
  * EVENT LISTENERS *
  *******************/
+const d6 = document.querySelector('#d6-roll');
+const d12 = document.querySelector('#d12-roll')
+const d20 = document.querySelector('#d20-roll')
+
+d6.src = './images/start/d6.png';
+d6.addEventListener('click', rollD6)
+
+document.querySelector('#double-d6-roll-1').src = './images/start/d6.png';
+document.querySelector('#double-d6-roll-2').src = './images/start/d6.png';
+document.querySelector('#double-d6-roll-1').addEventListener('click', rollDoubleD6)
+document.querySelector('#double-d6-roll-2').addEventListener('click', rollDoubleD6)
+
+d12.src = './images/start/d12.jpeg';
+d12.addEventListener('click', rollTwelves)
+
+d20.src = './images/start/d20.jpg';
+d20.addEventListener('click', rollTwenty)
 
 
 /****************
